ab7ba3717baf9b92090a6b58c47968d0c7fef03e
Lae-Peleti <lpeleti@gov.tv>
2024-08-02 15:09:26 +1200

    added processing of death data and tabulation 2/8/2024

--- bd_processing.R
+++ bd_processing.R
@@ -52,6 +52,7 @@ births$island[births$island=="FUNAFUTI"] <- "Funafuti"
 births$island[births$island=="funafuti"] <- "Funafuti"
 births$island[births$island=="FUNFUTI"] <- "Funafuti"
 births$island[births$island=="Funfuti"] <- "Funafuti"
+births$island[births$island=="F unafuti"] <- "Funafuti"
 births$island[is.na(births$island)] <- "NS"
 
 #age and age group of mothers
@@ -84,5 +85,70 @@ births$marriedStat[births$marriedStat=="married"] <- "Married"
 births$marriedStat[births$marriedStat=="Singlle"] <- "Single"
 #Count variable
 births$N <- 1
-
 dbWriteTable(mydb, "births", births, overwrite = TRUE)
+#--------------------------------------------------------------------------------------
+#               DEATHS
+#--------------------------------------------------------------------------------------
+deaths <- read_excel("data/deaths.xlsx")
+
+deaths$id <- row_number(deaths$Quarter)
+deaths <- deaths[!is.na(deaths$Quarter), ]
+
+#Date of death, year of death, and month of death
+colnames(deaths)[colnames(deaths) == "Date of Death"] <- "DOD"
+deaths$date <- convertToDateTime(deaths$DOD, origin = "1900-01-01")
+deaths$yearDeath <- year(deaths$date)
+deaths$monthDeath <- month(deaths$date)
+deaths$monthDeath[is.na(deaths$monthDeath)] <- "NS"
+
+#Sex
+#Manually changing unknown sex for Iona Tinapa
+colnames(deaths)[colnames(deaths) == "Name of Deceased"] <- "name"
+deaths$Sex <- ifelse(deaths$name == "Iona" & deaths$Surname == "Tinapa",1,deaths$Sex)
+deaths$Sex[deaths$Sex==1] <- "Male"
+deaths$Sex[deaths$Sex==2] <- "Female"
+
+#Age and age group
+colnames(deaths)[colnames(deaths) == "Date of Birth"] <- "DOB"
+deathsDOB <- dmy(deaths$DOB)
+deaths$DOB <- convertToDateTime(deaths$DOB, origin = "1900-01-01")
+deaths$yearBirth <- year(deaths$DOB)
+deaths$Age <- deaths$yearDeath - deaths$yearBirth
+deaths$ageGroup <- ifelse(deaths$Age <5, "0-4",
+                          ifelse(deaths$Age >= 5 & deaths$Age <=9,"5-9",
+                                 ifelse(deaths$Age >= 10 & deaths$Age <=14,"10-14",
+                                        ifelse(deaths$Age >= 15 & deaths$Age <=19,"15-19",
+                                               ifelse(deaths$Age >= 20 & deaths$Age <=24,"20-24",
+                                                      ifelse(deaths$Age >= 25 & deaths$Age <=29,"25-29",
+                                                             ifelse(deaths$Age >= 30 & deaths$Age <=34,"30-34",
+                                                                    ifelse(deaths$Age >= 35 & deaths$Age <=39,"35-39",
+                                                                           ifelse(deaths$Age >= 40 & deaths$Age <=44,"40-44",
+                                                                                  ifelse(deaths$Age >= 45 & deaths$Age <=49,"45-49",
+                                                                                         ifelse(deaths$Age >= 50 & deaths$Age <=54,"50-54",
+                                                                                                ifelse(deaths$Age >= 55 & deaths$Age <=59,"55-59",
+                                                                                                       ifelse(deaths$Age >= 60 & deaths$Age <=64,"60-64",
+                                                                                                              ifelse(deaths$Age >= 65 & deaths$Age <=69,"65-69",
+                                                                                                                     ifelse(deaths$Age >= 70 & deaths$Age <=74,"70-74",
+                                                                                                                            ifelse(deaths$Age >= 75 & deaths$Age <=79,"75-79",
+                                                                                                                                   ifelse(deaths$Age >= 80 & deaths$Age <=84,"80-84",
+                                                                                                                                          ifelse(deaths$Age >= 85 & deaths$Age <=89,"85-89",
+                                                                                                                                                 ifelse(deaths$Age >= 90,"90+","NS")))))))))))))))))))
+deaths$ageGroup[is.na(deaths$ageGroup)] <- "NS"
+
+#island
+colnames(deaths)[colnames(deaths) == "Island of occurrence"] <- "island"
+deaths$island[deaths$island=="FUNAFUTI"] <- "Funafuti"
+deaths$island[deaths$island=="Fuanfuti"] <- "Funafuti"
+deaths$island[deaths$island=="NIUTAO"] <- "Niutao"
+deaths$island[deaths$island=="NMAGA"] <- "Nanumaga"
+
+#place of death
+colnames(deaths)[colnames(deaths) == "Place of death"] <- "place"
+deaths$place[deaths$place=="HOME"] <- "Home"
+deaths$place[deaths$place=="Hh"] <- "Home"
+deaths$place[deaths$place=="Hopsital"] <- "Hospital"
+deaths$place[deaths$place=="HOSPITAL"] <- "Hospital"
+deaths$place[deaths$place=="Out Island"] <- "Out island"
+
+deaths$N <- 1
+dbWriteTable(mydb, "deaths", deaths, overwrite = TRUE)
--- bd_tabulation.R
+++ bd_tabulation.R
@@ -23,70 +23,126 @@ setwd(repository)
 #Connect to db
 mydb <- dbConnect(RSQLite::SQLite(), "data/vital.db")
 wb <- createWorkbook(creator = Sys.getenv("USERNAME"))
-t1 <- dbGetQuery(mydb, "SELECT * FROM births WHERE quarter = 1")
+t1 <- dbGetQuery(mydb, "SELECT * FROM births WHERE monthBirth <= 6")
 
 #-----------------------------------------------------
-#Table 1 Births by month and sex
+#Table B1 Births by month and sex
 #-----------------------------------------------------
 pt <- PivotTable$new()
 pt$addData(t1)
 pt$addColumnDataGroups("sex")
 pt$addRowDataGroups("monthBirth")
-pt$defineCalculation(calculationName="TotalVisitors", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$defineCalculation(calculationName="totalBirths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
 pt$renderPivot()
 addWorksheet(wb, "tableB1")
 pt$writeToExcelWorksheet(wb=wb, wsName="tableB1", 
                          topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
 #-----------------------------------------------------
-#Table 2 Births by island and sex
+#Table B2 Births by island and sex
 #-----------------------------------------------------
 pt <- PivotTable$new()
 pt$addData(t1)
 pt$addColumnDataGroups("sex")
 pt$addRowDataGroups("island")
-pt$defineCalculation(calculationName="TotalVisitors", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$defineCalculation(calculationName="totalBirths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
 pt$renderPivot()
 addWorksheet(wb, "tableB2")
 pt$writeToExcelWorksheet(wb=wb, wsName="tableB2", 
                          topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
 
 #-----------------------------------------------------
-#Table 3 Births by age group and marital status
+#Table B3 Births by age group and marital status
 #-----------------------------------------------------
 pt <- PivotTable$new()
 pt$addData(t1)
 pt$addColumnDataGroups("marriedStat")
 pt$addRowDataGroups("ageGroup")
-pt$defineCalculation(calculationName="TotalVisitors", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$defineCalculation(calculationName="totalBirths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
 pt$renderPivot()
 addWorksheet(wb, "tableB3")
 pt$writeToExcelWorksheet(wb=wb, wsName="tableB3", 
                          topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
 
 #-----------------------------------------------------
-#Table 4 Births by age month and place of birth
+#Table B4 Births by age month and place of birth
 #-----------------------------------------------------
 pt <- PivotTable$new()
 pt$addData(t1)
 pt$addColumnDataGroups("placeBirth")
 pt$addRowDataGroups("monthBirth")
-pt$defineCalculation(calculationName="TotalVisitors", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$defineCalculation(calculationName="totalBirths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
 pt$renderPivot()
 addWorksheet(wb, "tableB4")
 pt$writeToExcelWorksheet(wb=wb, wsName="tableB4", 
                          topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
 
 #-----------------------------------------------------
-#Table 5 Births by age month and marital status
+#Table B5 Births by age month and marital status
 #-----------------------------------------------------
 pt <- PivotTable$new()
 pt$addData(t1)
 pt$addColumnDataGroups("marriedStat")
 pt$addRowDataGroups("monthBirth")
-pt$defineCalculation(calculationName="TotalVisitors", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$defineCalculation(calculationName="totalBirths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
 pt$renderPivot()
 addWorksheet(wb, "tableB5")
 pt$writeToExcelWorksheet(wb=wb, wsName="tableB5", 
                          topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
 
-saveWorkbook(wb, file="output/Births Tables.xlsx", overwrite = TRUE)
+#-----------------------------------------------------
+#Table D1 Deaths by month and sex
+#-----------------------------------------------------
+t1 <- dbGetQuery(mydb, "SELECT * FROM deaths WHERE monthDeath <= 6")
+pt <- PivotTable$new()
+pt$addData(t1)
+pt$addColumnDataGroups("Sex")
+pt$addRowDataGroups("monthDeath")
+pt$defineCalculation(calculationName="totalDeaths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$renderPivot()
+addWorksheet(wb, "tableD1")
+pt$writeToExcelWorksheet(wb=wb, wsName="tableD1", 
+                         topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
+
+#-----------------------------------------------------
+#Table D2 Deaths by age group and sex
+#-----------------------------------------------------
+t1 <- dbGetQuery(mydb, "SELECT * FROM deaths WHERE monthDeath <= 6")
+pt <- PivotTable$new()
+pt$addData(t1)
+pt$addColumnDataGroups("Sex")
+pt$addRowDataGroups("ageGroup")
+pt$defineCalculation(calculationName="totalDeaths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$renderPivot()
+addWorksheet(wb, "tableD2")
+pt$writeToExcelWorksheet(wb=wb, wsName="tableD2", 
+                         topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
+
+#-----------------------------------------------------
+#Table D3 Deaths by island of occurrence and sex
+#-----------------------------------------------------
+t1 <- dbGetQuery(mydb, "SELECT * FROM deaths WHERE monthDeath <= 6")
+pt <- PivotTable$new()
+pt$addData(t1)
+pt$addColumnDataGroups("Sex")
+pt$addRowDataGroups("island")
+pt$defineCalculation(calculationName="totalDeaths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$renderPivot()
+addWorksheet(wb, "tableD3")
+pt$writeToExcelWorksheet(wb=wb, wsName="tableD3", 
+                         topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
+
+#-----------------------------------------------------
+#Table D4 Deaths by place of death and sex
+#-----------------------------------------------------
+t1 <- dbGetQuery(mydb, "SELECT * FROM deaths WHERE monthDeath <= 6")
+pt <- PivotTable$new()
+pt$addData(t1)
+pt$addColumnDataGroups("Sex")
+pt$addRowDataGroups("place")
+pt$defineCalculation(calculationName="totalDeaths", summariseExpression="format(round(sum(N), 0), big.mark = ',')")
+pt$renderPivot()
+addWorksheet(wb, "tableD4")
+pt$writeToExcelWorksheet(wb=wb, wsName="tableD4", 
+                         topRowNumber=1, leftMostColumnNumber=1, applyStyles=TRUE, mapStylesFromCSS=TRUE)
+
+saveWorkbook(wb, file="output/Vital Tables.xlsx", overwrite = TRUE)
